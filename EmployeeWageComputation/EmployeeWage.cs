88f3362c02729f95bedd482ae11d3c8d6917f16b
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-10 19:28:07 +0530

    [Vaibhav] Add:Attendance

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeWage
+    {
+        const int IS_PRESENT = 1;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
