5a617989bdff03259d9c61367c1a6971d28d808a
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-10 19:37:52 +0530

    [Vaibhav] Add:DailyEmpAWage

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT = 1;
+        const int IS_PRESENT = 1, WAGE_PER_HR = 20, FULL_DAY_HR = 8;
         public void Attendance()
         {
             Random random = new Random();
@@ -17,6 +17,11 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee is Present");
             else
                 Console.WriteLine("Employee is Absent");
+        }
+        public void DailyEmpWage()
+        {
+            int wage = WAGE_PER_HR * FULL_DAY_HR;
+            Console.WriteLine(wage);
         }
     }
 }
