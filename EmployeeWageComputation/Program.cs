dcd725bbcaa48b4e1872486c9c7dfc7a9a0290d8
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-04-27 20:10:57 +0530

    Update Program.cs

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,26 +7,6 @@ namespace EmployeeWageComputataion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation.");
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.Attendance();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculateWage();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculatingPartTimeWage();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculatingPartTimeWageWithSwitch();
-
-            Console.WriteLine("=============X=============");
-
             EmployeeDetails.CalculateWagesForMonth();
         }
     }
