0f01e4894a374e3ed8c45d86e4aee4bd186602d3
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-05 17:13:01 +0530

    Update Program.cs

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,36 +7,6 @@ namespace EmployeeWageComputataion
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Welcome to Employee Wage Computation.");
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.Attendance();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculateWage();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculatingPartTimeWage();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculatingPartTimeWageWithSwitch();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculateWagesForMonth();
-
-            Console.WriteLine("=============X=============");
-
-            EmployeeDetails.CalculateWageTillCondition();
-            */
-
-            //Refactor.ComputeEmployeeWage();
-
             MultipleCompanies abc = new MultipleCompanies();
             abc.ComputeEmpWage("Amazon", 20, 20, 8);
             abc.ComputeEmpWage("Facebook", 15, 20, 10);
