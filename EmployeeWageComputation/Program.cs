6fa7c8675c8cad270d8ef0cfb38c1165d915dbe4
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-10 16:40:53 +0530

    [Vaibhav] Add:Save total wage for each company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -37,9 +37,18 @@ namespace EmployeeWageComputataion
 
             //Refactor.ComputeEmployeeWage();
 
-            MultipleCompanies abc = new MultipleCompanies();
-            abc.ComputeEmpWage("Amazon", 20, 20, 8);
-            abc.ComputeEmpWage("Facebook", 15, 20, 10);
+            //MultipleCompanies abc = new MultipleCompanies();
+            //abc.ComputeEmpWage("Amazon", 20, 20, 8);
+            //abc.ComputeEmpWage("Facebook", 15, 20, 10);
+
+            SaveTotalWage google = new SaveTotalWage("Google", 20, 5, 160);
+            SaveTotalWage tata = new SaveTotalWage("Tata", 10, 5, 160);
+
+            google.computeEmpWage();
+            Console.WriteLine(google.toString());
+
+            tata.computeEmpWage();
+            Console.WriteLine(tata.toString());
         }
     }
 }
